287cac790fe36fa04a6752c72f38ab488bcab9b7
1103137208 <1103137208@gm.kuas.edu.tw>
2017-04-07 15:50:30 +0800

    0407

---
+++ eSaleDao/OrderDao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSaleDao
+{
+    public class OrderDao
+    {
+        public eSaleModel.Order GetOrderById(string id)
+        {
+            ///<summary>
+            ///依訂單編號取得訂單資料
+            ///</summary>
+            ///<param name="id"></param>
+            ///<returns><returns>
+            return new eSaleModel.Order() { CustId = "GSS", CustName = "瑞陽資訊", OrderId = "1" };
+        }
+    }
+}
